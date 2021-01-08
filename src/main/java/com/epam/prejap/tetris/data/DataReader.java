cc34d9aef59043f83e281d019ebb03bc9552d3ea
lukaszspyra <lukasz.spyrka@gmail.com>
2021-01-08 18:51:57 +0100

    Create DataReader reading data from .txt file
    DataReader uses Jackson library to map array of Jsons to array of HallOfFame object in the form of:
    - name : String name
    - points : int number of points

---
+++ src/main/java/com/epam/prejap/tetris/data/DataReader.java
@@ -0,0 +1,20 @@
+package com.epam.prejap.tetris.data;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+final class DataReader {
+
+    private final Path path;
+
+    DataReader(final Path path) {
+        this.path = path;
+    }
+
+    HallOfFame[] readFromFile() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(path.toFile(), HallOfFame[].class);
+    }
+}
