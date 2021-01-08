c1db9e92924611d6360e3dcd81254a76cbd46907
Maciej Krawczyk <krawmaciej@gmail.com>
2021-01-08 18:11:03 +0100

    Override equals and hashcode

--- src/main/java/com/epam/prejap/tetris/block/Point.java
+++ src/main/java/com/epam/prejap/tetris/block/Point.java
@@ -1,5 +1,7 @@
 package com.epam.prejap.tetris.block;
 
+import java.util.Objects;
+
 class Point {
     final int row;
     final int column;
@@ -7,6 +9,19 @@ class Point {
     Point(int row, int column) {
         this.row = row;
         this.column = column;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return row == point.row && column == point.column;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, column);
     }
 
 }
