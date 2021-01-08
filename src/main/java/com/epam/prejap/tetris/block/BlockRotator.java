cc7fdc6976e37c3b37d3836a030fa3c0db5a7c52
Maciej Krawczyk <krawmaciej@gmail.com>
2021-01-08 17:02:53 +0100

    Add BlockRotator and Point classes
    Point is used to hold row and column values for a dot inside an image
    BlockRotator will be used to return consecutive clockwise rotations of
    a block that it's initialized with:
    90 degrees, 180 degrees, 270 degrees 360 degrees-initial block position

---
+++ src/main/java/com/epam/prejap/tetris/block/BlockRotator.java
@@ -0,0 +1,50 @@
+package com.epam.prejap.tetris.block;
+
+public class BlockRotator {
+
+    private Block block;
+    private Point blockOffset;
+
+    public BlockRotator(Block block) {
+        this.block = block;
+    }
+
+
+    public void rotate() {
+        byte[][] rotatedImage = rotateBlockImage90DegreesClockwise();
+
+
+
+    }
+
+    private byte[][] rotateBlockImage90DegreesClockwise() {
+        int rotatedRows = block.cols();
+        int rotatedColumns = block.rows();
+        Point rotatedImageSize = new Point(rotatedRows, rotatedColumns);
+        byte[][] rotatedImage = emptyRotatedImage(rotatedImageSize);
+
+        for (int row = 0; row < block.rows(); row++) {
+            for (int column = 0; column < block.cols(); column++) {
+                Point toRotate = new Point(row, column);
+                rotatePointOnImage(rotatedImage, rotatedImageSize, toRotate);
+            }
+        }
+        return rotatedImage;
+    }
+
+    private byte[][] emptyRotatedImage(Point rotatedImageSize) {
+        return new byte[rotatedImageSize.row][rotatedImageSize.column];
+    }
+
+    private void rotatePointOnImage(byte[][] rotatedImage, Point rotatedImageSize, Point toRotate) {
+        Point rotated = rotatePoint90DegreesClockwise(toRotate, rotatedImageSize);
+        rotatedImage[rotated.row][rotated.column] = block.dotAt(toRotate.row, toRotate.column);
+    }
+
+    private Point rotatePoint90DegreesClockwise(Point toRotate, Point rotatedImageSize) {
+        int rotatedRow = toRotate.column;
+        int rotatedColumn = rotatedImageSize.column - 1 - toRotate.row;
+        return new Point(rotatedRow, rotatedColumn);
+    }
+    
+}
---
+++ src/main/java/com/epam/prejap/tetris/block/Point.java
@@ -0,0 +1,12 @@
+package com.epam.prejap.tetris.block;
+
+class Point {
+    final int row;
+    final int column;
+
+    Point(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+}
