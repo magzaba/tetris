3690bed5929e6f0d045d515888e38681a88757be
Maciej Krawczyk <krawmaciej@gmail.com>
2021-01-08 20:38:33 +0100

    Add getters and implement rotate() method

--- src/main/java/com/epam/prejap/tetris/block/BlockRotator.java
+++ src/main/java/com/epam/prejap/tetris/block/BlockRotator.java
@@ -3,13 +3,21 @@ package com.epam.prejap.tetris.block;
 public class BlockRotator {
 
     private Block block;
+    private Point blockOffset;
 
     private Point rotationPoint;
-    private Point blockOffset;
 
     public BlockRotator(Block block) {
         this.block = block;
         rotationPoint = blockRotationPoint();
+    }
+
+    public Block getRotatedBlock() {
+        return block;
+    }
+
+    public Point getBlockOffset() {
+        return blockOffset;
     }
 
     Point blockRotationPoint() {
@@ -19,16 +27,35 @@ public class BlockRotator {
     }
 
     public void rotate() {
-        byte[][] rotatedImage = rotateBlockImage90DegreesClockwise();
-
-
-
-    }
-
-    private byte[][] rotateBlockImage90DegreesClockwise() {
         int rotatedRows = block.cols();
         int rotatedColumns = block.rows();
         Point rotatedImageSize = new Point(rotatedRows, rotatedColumns);
+
+        byte[][] rotatedImage = rotateBlockImage90DegreesClockwise(rotatedImageSize);
+        blockOffset = setNewRotationPoint(rotatedImageSize);
+
+        block = block.copyWithImage(rotatedImage);
+    }
+
+    /**
+     * Calculates and sets {@link BlockRotator#rotationPoint} to rotation point on a rotated block.
+     * @param   rotatedImageSize size of a rotated image to which rotation point will be rotated
+     * @return  an offset(distance) between rotation point before and after rotation
+     */
+    private Point setNewRotationPoint(Point rotatedImageSize) {
+        Point newRotationPoint = rotatePointOnRotatedImage(rotationPoint, rotatedImageSize);
+        var blockOffset = calculateOffset(rotationPoint, newRotationPoint);
+        rotationPoint = newRotationPoint;
+        return blockOffset;
+    }
+
+    private Point calculateOffset(Point from, Point to) {
+        int row = from.row - to.row;
+        int column = from.column - to.column;
+        return new Point(row, column);
+    }
+
+    private byte[][] rotateBlockImage90DegreesClockwise(Point rotatedImageSize) {
         byte[][] rotatedImage = emptyRotatedImage(rotatedImageSize);
 
         for (int row = 0; row < block.rows(); row++) {
