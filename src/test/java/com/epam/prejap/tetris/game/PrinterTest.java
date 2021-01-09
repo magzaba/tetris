fdacb44aeee3f715e5ee60b1c75162ac9a6d68a3
lukaszspyra <lukasz.spyrka@gmail.com>
2021-01-09 19:51:32 +0100

    Add tests for Printer
    Tests for methods hallOfFame() and readInitials()

--- src/test/java/com/epam/prejap/tetris/game/PrinterTest.java
+++ src/test/java/com/epam/prejap/tetris/game/PrinterTest.java
@@ -1,14 +1,21 @@
 package com.epam.prejap.tetris.game;
 
+import com.epam.prejap.tetris.data.HallOfFame;
+import com.epam.prejap.tetris.data.HallOfFameMember;
 import org.mockito.Mockito;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
-import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.*;
 
 @Test(groups = "Timer")
 public class PrinterTest {
@@ -46,5 +53,67 @@ public class PrinterTest {
         // then
         Mockito.verify(printer).header();
         assertTrue(bos.toString().contains(message));
+    }
+
+    @Test(dataProvider = "mockHallOfFame30Members")
+    public void hallOfFameShallPrintMax25Entries(HallOfFameMember[] mockMembers){
+        //given
+        SoftAssert softAssert = new SoftAssert();
+        Timer timer = new Timer(1);
+        Printer printer = new Printer(new PrintStream(bos), timer);
+
+        //when
+        printer.hallOfFame(new ArrayList<>(Arrays.asList(mockMembers)));
+
+        //then
+        softAssert.assertTrue(bos.toString().contains("1"));
+        softAssert.assertTrue(bos.toString().contains("25"));
+        softAssert.assertFalse(bos.toString().contains("26"));
+        softAssert.assertFalse(bos.toString().contains("30"));
+        softAssert.assertAll("Shall print only 25 elements, but it did not");
+
+    }
+
+    @Test(dataProvider = "scannerMoreThan3CharsInput", expectedExceptions = InputMismatchException.class)
+    public void shallNotReadNameWithMoreThan3Chars(String name){
+        //given
+        Timer timer = new Timer(1);
+        Printer printer = new Printer(new PrintStream(bos), timer);
+        Scanner in = new Scanner(name);
+
+        //when
+        HallOfFameMember actual = printer.readInitials(1, in);
+
+        //then
+        assertThrows(InputMismatchException::new);
+    }
+
+    @DataProvider()
+    public static Object[] mockHallOfFame30Members() {
+        HallOfFameMember[] mock1 = new HallOfFameMember[30];
+        Arrays.fill(mock1, new HallOfFameMember("one", 1));
+        return new Object[]{mock1};
+    }
+
+    @DataProvider()
+    public static Object[] scanner3CharsInput() {
+        return new Object[]{
+                "one",
+                "two",
+                "123",
+                "...",
+                "!`!"
+        };
+    }
+
+    @DataProvider()
+    public static Object[] scannerMoreThan3CharsInput() {
+        return new Object[]{
+                "oneone",
+                "twotwo",
+                "123123",
+                "......",
+                "!`!!`!"
+        };
     }
 }
