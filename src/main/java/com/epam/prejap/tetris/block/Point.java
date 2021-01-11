cde9548c6fd95a8f3c9618d9a4a8e259dd328e47
Maciej Krawczyk <krawmaciej@gmail.com>
2021-01-11 01:32:03 +0100

    Add subtract and negate methods

--- src/main/java/com/epam/prejap/tetris/block/Point.java
+++ src/main/java/com/epam/prejap/tetris/block/Point.java
@@ -1,3 +1,10 @@
 package com.epam.prejap.tetris.block;
 
-public record Point(int row, int column) { }
+public record Point(int row, int column) {
+    public Point subtract(Point point) {
+        return new Point(this.row - point.row, this.column - column);
+    }
+    public Point negate() {
+        return new Point(-this.row, -this.column);
+    }
+}
