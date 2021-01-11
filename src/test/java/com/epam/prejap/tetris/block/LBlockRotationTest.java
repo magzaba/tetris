a419d5d6e50139b446f488dc54903d569a11a939
Maciej Krawczyk <krawmaciej@gmail.com>
2021-01-11 03:37:06 +0100

    Update tests to reflect rotation refactor

--- src/test/java/com/epam/prejap/tetris/block/LBlockRotationTest.java
+++ src/test/java/com/epam/prejap/tetris/block/LBlockRotationTest.java
@@ -3,8 +3,6 @@ package com.epam.prejap.tetris.block;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.util.stream.IntStream;
-
 import static org.testng.Assert.assertEquals;
 
 @Test(groups = "BlockRotation")
@@ -39,45 +37,45 @@ public class LBlockRotationTest {
     }
 
     @DataProvider
-    public Object[][] LBlockRotationOffsets() {
+    public Object[][] LBlockRotationPoints() {
         return new Object[][] {
-                {new Point(0, 0), 1},
-                {new Point(0, 1), 2},
-                {new Point(1, -1), 3},
-                {new Point(-1, 0), 4},
+                {new ArrayPoint(1, 1), 1},
+                {new ArrayPoint(1, 0), 2},
+                {new ArrayPoint(0, 1), 3},
+                {new ArrayPoint(1, 1), 4},
         };
     }
 
     @Test(dataProvider = "LBlockRotations")
     public void blockImageShouldBeRotated(byte[][] image, int howManyRotations) {
         // given
-        var block = new LBlock();
-        var blockRotator = new BlockRotator(block);
+        var block = RotatableBlock.of(new LBlock());
 
         // when
-        IntStream.range(0, howManyRotations).forEach(i -> blockRotator.rotate());
-        var rotatedBlock = blockRotator.getRotatedBlock();
+        for (int i = 0; i < howManyRotations; i++) {
+            block = block.rotate();
+        }
 
         // then
-        for (int i = 0; i < rotatedBlock.rows; i++) {
-            for (int j = 0; j < rotatedBlock.cols; j++) {
-                assertEquals(image[i][j], rotatedBlock.dotAt(i, j));
+        for (int i = 0; i < block.rows(); i++) {
+            for (int j = 0; j < block.cols(); j++) {
+                assertEquals(image[i][j], block.dotAt(i, j));
             }
         }
     }
 
-    @Test(dataProvider = "LBlockRotationOffsets")
-    public void blockOffsetsShouldReflectRotation(Point offset, int howManyRotations) {
+    @Test(dataProvider = "LBlockRotationPoints")
+    public void blockOffsetsShouldReflectRotation(ArrayPoint expectedRotationPoints, int howManyRotations) {
         // given
-        var block = new LBlock();
-        var blockRotator = new BlockRotator(block);
+        var block = RotatableBlock.of(new LBlock());
 
         // when
-        IntStream.range(0, howManyRotations).forEach(i -> blockRotator.rotate());
-        Point result = blockRotator.getBlockOffset();
+        for (int i = 0; i < howManyRotations; i++) {
+            block = block.rotate();
+        }
 
         // then
-        assertEquals(result, offset);
+        assertEquals(block.rotationPoint(), expectedRotationPoints);
     }
 
 }
