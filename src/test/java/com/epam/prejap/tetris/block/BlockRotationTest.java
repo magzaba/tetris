de42938820f48bbed4ddcf1419e15971822e751c
Maciej Krawczyk <krawmaciej@gmail.com>
2021-01-12 15:07:55 +0100

    Refactor existing rotation tests to use added factory

---
+++ src/test/java/com/epam/prejap/tetris/block/BlockRotationTest.java
@@ -0,0 +1,69 @@
+package com.epam.prejap.tetris.block;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+@Test(groups = "BlockRotation")
+public class BlockRotationTest {
+
+    private enum RotationDegree {
+        ZERO, NINETY, ONE_HUNDRED_EIGHTY, TWO_HUNDRED_SEVENTY, THREE_HUNDRED_SIXTY
+    }
+
+    @DataProvider
+    public Object[] rotationDegrees() {
+        return RotationDegree.values();
+    }
+
+    private final Block block;
+    private final Object[] rotationImages;
+    private final ArrayPoint[] rotationPoints;
+
+    public BlockRotationTest(Block block, Object[] rotationImages, ArrayPoint[] rotationPoints) {
+        this.block = block;
+        this.rotationImages = rotationImages;
+        this.rotationPoints = rotationPoints;
+    }
+
+    @Test(dataProvider = "rotationDegrees")
+    public void shallRotateBlockImage(RotationDegree degree) {
+        // given
+        var block = RotatableBlock.of(this.block);
+        var expectedRotatedImage = (byte[][])rotationImages[degree.ordinal()%4];
+
+        // when
+        var rotatedBlock = rotateBlockByDegrees(block, degree);
+
+        // then
+        for (int i = 0; i < rotatedBlock.rows(); i++) {
+            for (int j = 0; j < rotatedBlock.cols(); j++) {
+                assertEquals(rotatedBlock.dotAt(i, j), expectedRotatedImage[i][j],
+                        "Block image should have been rotated but did not");
+            }
+        }
+    }
+
+    @Test(dataProvider = "rotationDegrees")
+    public void shallRotateBlockRotationPoint(RotationDegree degree) {
+        // given
+        var block = RotatableBlock.of(new LBlock());
+        var expectedRotationPoint = rotationPoints[degree.ordinal()%4];
+
+        // when
+        var rotatedBlock = rotateBlockByDegrees(block, degree);
+
+        // then
+        assertEquals(rotatedBlock.rotationPoint(), expectedRotationPoint,
+                "Block rotation point should have been rotated " + degree.toString() + " times but did not");
+    }
+
+    private RotatableBlock rotateBlockByDegrees(RotatableBlock block, RotationDegree degree) {
+        for (int i = 0; i < degree.ordinal(); i++) {
+            block = block.rotate();
+        }
+        return block;
+    }
+
+}
---
+++ src/test/java/com/epam/prejap/tetris/block/BlockRotations.java
@@ -0,0 +1,63 @@
+package com.epam.prejap.tetris.block;
+
+class BlockRotations {
+
+    static final Object[] L_BLOCK_ROTATION_IMAGES = new Object[]{
+            new byte[][] {
+                    {1, 0},
+                    {1, 0},
+                    {1, 1},
+            },
+            new byte[][] {
+                    {1, 1, 1},
+                    {1, 0, 0},
+            },
+            new byte[][] {
+                    {1, 1},
+                    {0, 1},
+                    {0, 1},
+            },
+            new byte[][] {
+                    {0, 0, 1},
+                    {1, 1, 1},
+            },
+    };
+
+    static final ArrayPoint[] L_BLOCK_ROTATION_POINTS = new ArrayPoint[]{
+            new ArrayPoint(1, 1),
+            new ArrayPoint(1, 1),
+            new ArrayPoint(1, 0),
+            new ArrayPoint(0, 1),
+    };
+
+    static final Object[][] I_BLOCK_ROTATION_IMAGES = new Object[][]{
+            {},
+            {},
+            {},
+    };
+
+    static final ArrayPoint[] I_BLOCK_ROTATION_POINTS = new ArrayPoint[]{
+
+    };
+
+    static final Object[][] O_BLOCK_ROTATION_IMAGES = new Object[][]{
+            {},
+            {},
+            {},
+    };
+
+    static final ArrayPoint[] O_BLOCK_ROTATION_POINTS = new ArrayPoint[]{
+
+    };
+
+    static final Object[][] J_BLOCK_ROTATION_IMAGES = new Object[][]{
+            {},
+            {},
+            {},
+    };
+
+    static final ArrayPoint[] J_BLOCK_ROTATION_POINTS = new ArrayPoint[]{
+
+    };
+
+}
