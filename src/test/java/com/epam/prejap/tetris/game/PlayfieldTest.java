29b4003a01528fdcea7f97193cf9aa5132403faa
Maciej Krawczyk <krawmaciej@gmail.com>
2021-01-14 16:05:16 +0100

    Make PlayfieldTest test only 3 basic moves
    Testing all possible moves in PlayfieldTest made tests
    sometimes not pass, because rotation was included.
    Same thing will happen if drop block down move will be added,
    because of that I set this test to test only NONE, RIGHT and LEFT moves.

--- src/test/java/com/epam/prejap/tetris/game/PlayfieldTest.java
+++ src/test/java/com/epam/prejap/tetris/game/PlayfieldTest.java
@@ -24,7 +24,7 @@ public class PlayfieldTest {
         playfield = new Playfield(feed, printer, grid);
     }
 
-    @Test(dataProvider = "arrayOfMoves")
+    @Test(dataProvider = "arrayOfTestedMoves")
     public void givenEmptyGrid_moveCalledProperAmountOfTimes(Move move) {
         playfield.nextBlock();
         int blockStart = 0;
@@ -53,10 +53,11 @@ public class PlayfieldTest {
     }
 
     @DataProvider
-    public Object[][] arrayOfMoves() {
+    public Object[][] arrayOfTestedMoves() {
         var amtOfIterations = 15;
-        var amtOfPossibleMoves = Move.values().length;
-        return IntStream.rangeClosed(0, amtOfIterations).mapToObj(i -> new Object[]{Move.values()[i % amtOfPossibleMoves]}).toArray(Object[][]::new);
+        Move[] testedMoves = new Move[]{Move.NONE, Move.LEFT, Move.RIGHT};
+        var amtOfTestedMoves = testedMoves.length;
+        return IntStream.rangeClosed(0, amtOfIterations).mapToObj(i -> new Object[]{testedMoves[i % amtOfTestedMoves]}).toArray(Object[][]::new);
     }
 
 }
