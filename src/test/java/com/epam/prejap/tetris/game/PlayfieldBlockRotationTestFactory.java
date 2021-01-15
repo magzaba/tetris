be02afdc96602860d4825fc9c85a0ff59ef7baa3
Maciej Krawczyk <krawmaciej@gmail.com>
2021-01-15 14:11:20 +0100

    Add other blocks in game rotation tests

--- src/test/java/com/epam/prejap/tetris/game/PlayfieldBlockRotationTestFactory.java
+++ src/test/java/com/epam/prejap/tetris/game/PlayfieldBlockRotationTestFactory.java
@@ -7,8 +7,12 @@ public class PlayfieldBlockRotationTestFactory {
     @Factory
     public Object[] factoryMethod() {
         return new Object[]{
+                new PlayfieldBlockRotationTest(TestPlayfieldBlockRotations.iBlock()),
+                new PlayfieldBlockRotationTest(TestPlayfieldBlockRotations.jBlock()),
                 new PlayfieldBlockRotationTest(TestPlayfieldBlockRotations.lBlock()),
                 new PlayfieldBlockRotationTest(TestPlayfieldBlockRotations.oBlock()),
+                new PlayfieldBlockRotationTest(TestPlayfieldBlockRotations.sBlock()),
+                new PlayfieldBlockRotationTest(TestPlayfieldBlockRotations.zBlock()),
         };
     }
 
--- src/test/java/com/epam/prejap/tetris/game/TestPlayfieldBlockRotations.java
+++ src/test/java/com/epam/prejap/tetris/game/TestPlayfieldBlockRotations.java
@@ -10,134 +10,434 @@ record TestPlayfieldBlockRotations(BlockFeed mockBlockfeed,
 
     enum TimesUPKeyIsPressed {ZERO, ONE, TWO, THREE, FOUR}
 
-    private static final int GRID_ROWS = 7;
-    private static final int GRID_COLUMNS = 5;
+    private static final int GRID_ROWS = 8;
+    private static final int GRID_COLUMNS = 6;
+
+    private static final Object[][] I_BLOCK_ROTATIONS_ON_GRID = new Object[][]{
+            {
+                    TimesUPKeyIsPressed.ZERO, """
+                +------+
+                |  #   |
+                |  #   |
+                |  #   |
+                |  #   |
+                |      |
+                |      |
+                |      |
+                |      |
+                +------+"""
+            },
+            {
+                    TimesUPKeyIsPressed.ONE, """
+                +------+
+                |      |
+                |  ####|
+                |      |
+                |      |
+                |      |
+                |      |
+                |      |
+                |      |
+                +------+"""
+            },
+            {
+                    TimesUPKeyIsPressed.TWO, """
+                +------+
+                |      |
+                |      |
+                |  #   |
+                |  #   |
+                |  #   |
+                |  #   |
+                |      |
+                |      |
+                +------+"""
+            },
+            {
+                    TimesUPKeyIsPressed.THREE, """
+                +------+
+                |      |
+                |      |
+                |      |
+                |  ####|
+                |      |
+                |      |
+                |      |
+                |      |
+                +------+"""
+            },
+            {
+                    TimesUPKeyIsPressed.FOUR, """
+                +------+
+                |      |
+                |      |
+                |      |
+                |      |
+                |  #   |
+                |  #   |
+                |  #   |
+                |  #   |
+                +------+"""
+            },
+    };
+
+    private static final Object[][] J_BLOCK_ROTATIONS_ON_GRID = new Object[][]{
+            {
+                    TimesUPKeyIsPressed.ZERO, """
+                +------+
+                |   #  |
+                |   #  |
+                |  ##  |
+                |      |
+                |      |
+                |      |
+                |      |
+                |      |
+                +------+"""
+            },
+            {
+                    TimesUPKeyIsPressed.ONE, """
+                +------+
+                |      |
+                |  #   |
+                |  ### |
+                |      |
+                |      |
+                |      |
+                |      |
+                |      |
+                +------+"""
+            },
+            {
+                    TimesUPKeyIsPressed.TWO, """
+                +------+
+                |      |
+                |      |
+                |  ##  |
+                |  #   |
+                |  #   |
+                |      |
+                |      |
+                |      |
+                +------+"""
+            },
+            {
+                    TimesUPKeyIsPressed.THREE, """
+                +------+
+                |      |
+                |      |
+                |      |
+                |  ### |
+                |    # |
+                |      |
+                |      |
+                |      |
+                +------+"""
+            },
+            {
+                    TimesUPKeyIsPressed.FOUR, """
+                +------+
+                |      |
+                |      |
+                |      |
+                |      |
+                |   #  |
+                |   #  |
+                |  ##  |
+                |      |
+                +------+"""
+            },
+    };
 
     private static final Object[][] L_BLOCK_ROTATIONS_ON_GRID = new Object[][]{
             {
                     TimesUPKeyIsPressed.ZERO, """
-                +-----+
-                | #   |
-                | #   |
-                | ##  |
-                |     |
-                |     |
-                |     |
-                |     |
-                +-----+"""
+                +------+
+                |  #   |
+                |  #   |
+                |  ##  |
+                |      |
+                |      |
+                |      |
+                |      |
+                |      |
+                +------+"""
             },
             {
                     TimesUPKeyIsPressed.ONE, """
-                +-----+
-                |     |
-                | ### |
-                | #   |
-                |     |
-                |     |
-                |     |
-                |     |
-                +-----+"""
+                +------+
+                |      |
+                |  ### |
+                |  #   |
+                |      |
+                |      |
+                |      |
+                |      |
+                |      |
+                +------+"""
             },
             {
                     TimesUPKeyIsPressed.TWO, """
-                +-----+
-                |     |
-                |     |
-                | ##  |
-                |  #  |
-                |  #  |
-                |     |
-                |     |
-                +-----+"""
+                +------+
+                |      |
+                |      |
+                |  ##  |
+                |   #  |
+                |   #  |
+                |      |
+                |      |
+                |      |
+                +------+"""
             },
             {
                     TimesUPKeyIsPressed.THREE, """
-                +-----+
-                |     |
-                |     |
-                |     |
-                |   # |
-                | ### |
-                |     |
-                |     |
-                +-----+"""
+                +------+
+                |      |
+                |      |
+                |      |
+                |    # |
+                |  ### |
+                |      |
+                |      |
+                |      |
+                +------+"""
             },
             {
                     TimesUPKeyIsPressed.FOUR, """
-                +-----+
-                |     |
-                |     |
-                |     |
-                |     |
-                | #   |
-                | #   |
-                | ##  |
-                +-----+"""
+                +------+
+                |      |
+                |      |
+                |      |
+                |      |
+                |  #   |
+                |  #   |
+                |  ##  |
+                |      |
+                +------+"""
             },
     };
 
     private static final Object[][] O_BLOCK_ROTATIONS_ON_GRID = new Object[][]{
             {
                     TimesUPKeyIsPressed.ZERO, """
-                +-----+
-                | ##  |
-                | ##  |
-                |     |
-                |     |
-                |     |
-                |     |
-                |     |
-                +-----+"""
+                +------+
+                |  ##  |
+                |  ##  |
+                |      |
+                |      |
+                |      |
+                |      |
+                |      |
+                |      |
+                +------+"""
             },
             {
                     TimesUPKeyIsPressed.ONE, """
-                +-----+
-                |     |
-                | ##  |
-                | ##  |
-                |     |
-                |     |
-                |     |
-                |     |
-                +-----+"""
+                +------+
+                |      |
+                |  ##  |
+                |  ##  |
+                |      |
+                |      |
+                |      |
+                |      |
+                |      |
+                +------+"""
             },
             {
                     TimesUPKeyIsPressed.TWO, """
-                +-----+
-                |     |
-                |     |
-                | ##  |
-                | ##  |
-                |     |
-                |     |
-                |     |
-                +-----+"""
+                +------+
+                |      |
+                |      |
+                |  ##  |
+                |  ##  |
+                |      |
+                |      |
+                |      |
+                |      |
+                +------+"""
             },
             {
                     TimesUPKeyIsPressed.THREE, """
-                +-----+
-                |     |
-                |     |
-                |     |
-                | ##  |
-                | ##  |
-                |     |
-                |     |
-                +-----+"""
+                +------+
+                |      |
+                |      |
+                |      |
+                |  ##  |
+                |  ##  |
+                |      |
+                |      |
+                |      |
+                +------+"""
             },
             {
                     TimesUPKeyIsPressed.FOUR, """
-                +-----+
-                |     |
-                |     |
-                |     |
-                |     |
-                | ##  |
-                | ##  |
-                |     |
-                +-----+"""
+                +------+
+                |      |
+                |      |
+                |      |
+                |      |
+                |  ##  |
+                |  ##  |
+                |      |
+                |      |
+                +------+"""
             },
     };
+
+    private static final Object[][] S_BLOCK_ROTATIONS_ON_GRID = new Object[][]{
+            {
+                    TimesUPKeyIsPressed.ZERO, """
+                +------+
+                |  ##  |
+                | ##   |
+                |      |
+                |      |
+                |      |
+                |      |
+                |      |
+                |      |
+                +------+"""
+            },
+            {
+                    TimesUPKeyIsPressed.ONE, """
+                +------+
+                |      |
+                | #    |
+                | ##   |
+                |  #   |
+                |      |
+                |      |
+                |      |
+                |      |
+                +------+"""
+            },
+            {
+                    TimesUPKeyIsPressed.TWO, """
+                +------+
+                |      |
+                |      |
+                |  ##  |
+                | ##   |
+                |      |
+                |      |
+                |      |
+                |      |
+                +------+"""
+            },
+            {
+                    TimesUPKeyIsPressed.THREE, """
+                +------+
+                |      |
+                |      |
+                |      |
+                | #    |
+                | ##   |
+                |  #   |
+                |      |
+                |      |
+                +------+"""
+            },
+            {
+                    TimesUPKeyIsPressed.FOUR, """
+                +------+
+                |      |
+                |      |
+                |      |
+                |      |
+                |  ##  |
+                | ##   |
+                |      |
+                |      |
+                +------+"""
+            },
+    };
+
+    private static final Object[][] Z_BLOCK_ROTATIONS_ON_GRID = new Object[][]{
+            {
+                    TimesUPKeyIsPressed.ZERO, """
+                +------+
+                | ##   |
+                |  ##  |
+                |      |
+                |      |
+                |      |
+                |      |
+                |      |
+                |      |
+                +------+"""
+            },
+            {
+                    TimesUPKeyIsPressed.ONE, """
+                +------+
+                |      |
+                |  #   |
+                | ##   |
+                | #    |
+                |      |
+                |      |
+                |      |
+                |      |
+                +------+"""
+            },
+            {
+                    TimesUPKeyIsPressed.TWO, """
+                +------+
+                |      |
+                |      |
+                | ##   |
+                |  ##  |
+                |      |
+                |      |
+                |      |
+                |      |
+                +------+"""
+            },
+            {
+                    TimesUPKeyIsPressed.THREE, """
+                +------+
+                |      |
+                |      |
+                |      |
+                |  #   |
+                | ##   |
+                | #    |
+                |      |
+                |      |
+                +------+"""
+            },
+            {
+                    TimesUPKeyIsPressed.FOUR, """
+                +------+
+                |      |
+                |      |
+                |      |
+                |      |
+                | ##   |
+                |  ##  |
+                |      |
+                |      |
+                +------+"""
+            },
+    };
+
+    static TestPlayfieldBlockRotations iBlock() {
+        return new TestPlayfieldBlockRotations(
+                TestMockBlockFeed.iBlockFeed(),
+                I_BLOCK_ROTATIONS_ON_GRID,
+                GRID_ROWS,
+                GRID_COLUMNS
+        );
+    }
+
+    static TestPlayfieldBlockRotations jBlock() {
+        return new TestPlayfieldBlockRotations(
+                TestMockBlockFeed.jBlockFeed(),
+                J_BLOCK_ROTATIONS_ON_GRID,
+                GRID_ROWS,
+                GRID_COLUMNS
+        );
+    }
 
     static TestPlayfieldBlockRotations lBlock() {
         return new TestPlayfieldBlockRotations(
@@ -152,6 +452,24 @@ record TestPlayfieldBlockRotations(BlockFeed mockBlockfeed,
         return new TestPlayfieldBlockRotations(
                 TestMockBlockFeed.oBlockFeed(),
                 O_BLOCK_ROTATIONS_ON_GRID,
+                GRID_ROWS,
+                GRID_COLUMNS
+        );
+    }
+
+    static TestPlayfieldBlockRotations sBlock() {
+        return new TestPlayfieldBlockRotations(
+                TestMockBlockFeed.sBlockFeed(),
+                S_BLOCK_ROTATIONS_ON_GRID,
+                GRID_ROWS,
+                GRID_COLUMNS
+        );
+    }
+
+    static TestPlayfieldBlockRotations zBlock() {
+        return new TestPlayfieldBlockRotations(
+                TestMockBlockFeed.zBlockFeed(),
+                Z_BLOCK_ROTATIONS_ON_GRID,
                 GRID_ROWS,
                 GRID_COLUMNS
         );
